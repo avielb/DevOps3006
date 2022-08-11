96b6cfc8927ee61610d0eb6e2b46a78e45e5fc59
Aviel Buskila <aviel33@gmail.com>
2022-08-11 20:05:47 +0300

    Update 13.py

--- 13.py
+++ 13.py
@@ -5,7 +5,8 @@ def save_name(names_file):
     my_file.write(current_name + "\n")
     my_file.close()
 
-
+dev haim(avi):
+    print(avi)
 def show_names(names_file, blabla):
     my_file,a  = open(names_file, "r"), "a"
     aviel = "david"
